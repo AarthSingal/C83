a05a326ab2a13fe36c4345faa020ad3a42d52396
AarthSingal <70443223+AarthSingal@users.noreply.github.com>
2021-09-23 14:23:30 +0530

    Add files via upload

---
+++ components/MyHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header,Icon } from 'react-native-elements';
+
+const MyHeader=props=>{
+    return(
+    <Header 
+    centerComponent={{text:props.title,style:{color : 'yellow',fontSize : 20, fontWeight : 'bold'}}}
+    backgroundColor="black"/>)
+}
+export default MyHeader;
